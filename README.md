9505445d7c4bb53629aec0987c7ce910ea941ad3
Your Name <kfhohn@gmail.com>
2023-07-27 12:06:17 -0400

    student list page loads from spreadsheet, rows return netid on click

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 PEOPLESOFT FRIEND PORTAL
+
+pre-populated link for incident reporting form:
+https://docs.google.com/forms/d/e/1FAIpQLScx5G-r9tsFE9_VQc4u3c50s7krQgD22tYl_hTYRxofYKsUQg/viewform?usp=pp_url&entry.623932798=kfh237
+
+pre-populated link for suspension form:
+https://docs.google.com/forms/d/e/1FAIpQLSfGyrqZ4MZQz_iJWzjogJoBK7yCHZVUVcmVGncMQddlG2YVVw/viewform?usp=pp_url&entry.917868986=kfh237
--- src/sheets.js
+++ src/sheets.js
@@ -1,13 +1,12 @@
-let ss = SpreadsheetApp.openById('1h2mZPw-CIv0ajfLYrGS0lpYUk8yAM-_OKBFK_YSTzEk');
+const ss = SpreadsheetApp.openById('1h2mZPw-CIv0ajfLYrGS0lpYUk8yAM-_OKBFK_YSTzEk');
 
 //get data from sheets
 function getStudentList() {
-  let sheet = ss.getSheetByName('Signups'),
+  const sheet = ss.getSheetByName('Signups'),
     lr = sheet.getLastRow(),
     lc = sheet.getLastColumn(),
-    // students = sheet.getRange(2, 1, lr, lc).getValues();
-    students = sheet.getRange('A:J').getValues();
+    students = sheet.getRange(2, 1, lr, lc).getValues();
 
-    Logger.log(JSON.stringify(students));
-    return sheet.getRange('A:J').getValues();
+  Logger.log(JSON.stringify(students));
+  return JSON.stringify(students);
 }
